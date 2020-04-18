f8596895aedb49f02f15ebfa3d1f7bc89f4625b3
Praveen Bisht <prvnbist@gmail.com>
2020-04-18 21:57:43 +0530

    feat: apollo client integration

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ require('dotenv').config()
 module.exports = {
    env: {
       HASURA_KEY: process.env.HASURA_KEY,
+      GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
    },
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,41 @@
 import React from 'react'
 import App from 'next/app'
 import '../global.css'
+import fetch from 'node-fetch'
+import { ApolloLink, concat } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloClient } from 'apollo-client'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = new HttpLink({
+   fetch: fetch,
+   uri: process.env.GRAPHQL_ENDPOINT,
+})
+
+const middlewareLink = new ApolloLink((operation, forward) => {
+   operation.setContext({
+      headers: {
+         'Content-Type': 'application/json',
+         'x-hasura-admin-secret': process.env.HASURA_KEY,
+      },
+   })
+   return forward(operation)
+})
+
+const client = new ApolloClient({
+   link: concat(middlewareLink, httpLink),
+   cache: new InMemoryCache(),
+})
 
 class MyApp extends App {
    render() {
       const { Component, pageProps } = this.props
-      return <Component {...pageProps} />
+      return (
+         <ApolloProvider client={client}>
+            <Component {...pageProps} />
+         </ApolloProvider>
+      )
    }
 }
 
