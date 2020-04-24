eaf64874acb44f1ae78e100fbbb8d0b548616c29
Praveen Bisht <prvnbist@gmail.com>
2020-04-24 23:38:41 +0530

    feat: responsive table

--- sections/analytics/index.jsx
+++ sections/analytics/index.jsx
@@ -9,27 +9,25 @@ export const Analytics = () => {
 
    if (loading) return <div>Loading...</div>
    return (
-      <div>
-         <div className="flex">
-            <div className="w-6/12 mr-6">
+      <div className="flex flex-col lg:flex-row lg:space-x-4">
+         <div className="w-full lg:w-6/12">
+            <h2 className="border-b pb-2 text-lg mt-3 text-teal-700">
+               Spendings by categories
+            </h2>
+            <ByCategories expenses={expenses} />
+         </div>
+         <div className="w-full lg:w-6/12">
+            <div className="w-full">
                <h2 className="border-b pb-2 text-lg mt-3 text-teal-700">
-                  Spendings by categories
+                  Spendings by Years
                </h2>
-               <ByCategories expenses={expenses} />
+               <ByYears expenses={expenses} />
             </div>
-            <div className="w-6/12">
-               <div className="w-full">
-                  <h2 className="border-b pb-2 text-lg mt-3 text-teal-700">
-                     Spendings by Years
-                  </h2>
-                  <ByYears expenses={expenses} />
-               </div>
-               <div className="w-full">
-                  <h2 className="border-b pb-2 text-lg mt-5 text-teal-700">
-                     Spendings by Months
-                  </h2>
-                  <ByMonths expenses={expenses} />
-               </div>
+            <div className="w-full">
+               <h2 className="border-b pb-2 text-lg mt-5 text-teal-700">
+                  Spendings by Months
+               </h2>
+               <ByMonths expenses={expenses} />
             </div>
          </div>
       </div>
