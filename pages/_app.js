40340e3f1205911bc82f1a411b9a583e9550fcf5
Praveen Bisht <prvnbist@gmail.com>
2021-03-28 15:59:56 +0530

    feat: apollo integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,73 @@
+import {
+   split,
+   ApolloClient,
+   ApolloLink,
+   InMemoryCache,
+   createHttpLink,
+   ApolloProvider,
+} from '@apollo/client'
+import fetch from 'node-fetch'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
+
+import '../styles/global.css'
 import GlobalStyles from '../styles/global'
 
-const App = ({ Component, pageProps }) => (
-   <>
-      <GlobalStyles />
-      <Component {...pageProps} />
-   </>
-)
+const wssLink = process.browser
+   ? new WebSocketLink(
+        new SubscriptionClient(`${process.env.WS_GRAPHQL_ENDPOINT}`, {
+           reconnect: true,
+           connectionParams: {
+              headers: {
+                 'x-hasura-admin-secret': `${process.env.HASURA_KEY}`,
+              },
+           },
+        })
+     )
+   : null
+
+const authLink = new ApolloLink((operation, forward) => {
+   operation.setContext(({ headers }) => ({
+      headers: {
+         ...headers,
+         'x-hasura-admin-secret': `${process.env.HASURA_KEY}`,
+      },
+   }))
+   return forward(operation)
+})
+
+const httpLink = createHttpLink({
+   fetch,
+   uri: `${process.env.GRAPHQL_ENDPOINT}`,
+})
+
+const splitLink = process.browser
+   ? split(
+        ({ query }) => {
+           const definition = getMainDefinition(query)
+           return (
+              definition.kind === 'OperationDefinition' &&
+              definition.operation === 'subscription'
+           )
+        },
+        wssLink,
+        authLink.concat(httpLink)
+     )
+   : authLink.concat(httpLink)
+
+const client = new ApolloClient({
+   link: splitLink,
+   cache: new InMemoryCache(),
+})
+
+const App = ({ Component, pageProps }) => {
+   return (
+      <ApolloProvider client={client}>
+         <GlobalStyles />
+         <Component {...pageProps} />
+      </ApolloProvider>
+   )
+}
 
 export default App
--- pages/index.js
+++ pages/index.js
@@ -5,43 +5,45 @@ import { Layout } from '../sections'
 import * as Icon from '../assets/icons'
 import { Button, Table } from '../components'
 
-const IndexPage = () => (
-   <Layout>
-      <Button.Group>
-         <Button.Icon>
-            <Icon.Delete tw="stroke-current" />
-         </Button.Icon>
-         <Button.Icon>
-            <Icon.Edit tw="stroke-current" />
-         </Button.Icon>
-      </Button.Group>
-      <Table>
-         <Table.Head>
-            <Table.Row>
-               <Table.HCell>Title</Table.HCell>
-               <Table.HCell>Category</Table.HCell>
-            </Table.Row>
-         </Table.Head>
-         <Table.Body>
-            <Table.Row odd>
-               <Table.Cell>Dinner</Table.Cell>
-               <Table.Cell>Food & Drinks</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-               <Table.Cell>Wifi</Table.Cell>
-               <Table.Cell>Internet/Talktime</Table.Cell>
-            </Table.Row>
-            <Table.Row odd>
-               <Table.Cell>Cab</Table.Cell>
-               <Table.Cell>Travel</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-               <Table.Cell>Netflix</Table.Cell>
-               <Table.Cell>Entertainment</Table.Cell>
-            </Table.Row>
-         </Table.Body>
-      </Table>
-   </Layout>
-)
+const IndexPage = () => {
+   return (
+      <Layout>
+         <Button.Group>
+            <Button.Icon>
+               <Icon.Delete tw="stroke-current" />
+            </Button.Icon>
+            <Button.Icon>
+               <Icon.Edit tw="stroke-current" />
+            </Button.Icon>
+         </Button.Group>
+         <Table>
+            <Table.Head>
+               <Table.Row>
+                  <Table.HCell>Title</Table.HCell>
+                  <Table.HCell>Category</Table.HCell>
+               </Table.Row>
+            </Table.Head>
+            <Table.Body>
+               <Table.Row odd>
+                  <Table.Cell>Dinner</Table.Cell>
+                  <Table.Cell>Food & Drinks</Table.Cell>
+               </Table.Row>
+               <Table.Row>
+                  <Table.Cell>Wifi</Table.Cell>
+                  <Table.Cell>Internet/Talktime</Table.Cell>
+               </Table.Row>
+               <Table.Row odd>
+                  <Table.Cell>Cab</Table.Cell>
+                  <Table.Cell>Travel</Table.Cell>
+               </Table.Row>
+               <Table.Row>
+                  <Table.Cell>Netflix</Table.Cell>
+                  <Table.Cell>Entertainment</Table.Cell>
+               </Table.Row>
+            </Table.Body>
+         </Table>
+      </Layout>
+   )
+}
 
 export default IndexPage
