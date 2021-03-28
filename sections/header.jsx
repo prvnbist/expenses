2c77fd6e7456ea58775d920d34d1eec383036360
Praveen Bisht <prvnbist@gmail.com>
2021-03-28 18:00:10 +0530

    feat: sticky header

--- sections/header.jsx
+++ sections/header.jsx
@@ -30,7 +30,7 @@ const Route = ({ path, title, is_active }) => (
 )
 
 const Styles = {
-   Header: tw.header`flex items-center justify-center h-auto p-2 border-b border-gray-700`,
+   Header: tw.header`z-10 bg-gray-800 sticky top-0 flex items-center justify-center h-auto p-2 border-b border-gray-700`,
    NavItems: tw.ul`h-full flex items-center gap-3 flex-wrap`,
    NavLink: styled.a`
       ${tw`cursor-pointer inline-flex items-center justify-center px-4 h-10 rounded-lg`}
