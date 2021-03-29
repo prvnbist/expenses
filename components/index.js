aeb45832e8507394300fda7042e4a22fa5ea55a4
Praveen Bisht <prvnbist@gmail.com>
2021-03-29 23:42:16 +0530

    feat(component): Loader

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export * from './button'
 export * from './table'
+export * from './loader'
 export * from './tableLoader'
---
+++ components/loader.js
@@ -0,0 +1,44 @@
+import tw, { styled } from 'twin.macro'
+
+export const Loader = () => <Styles.Loader>Loading...</Styles.Loader>
+
+const Styles = {
+   Loader: styled.div`
+      width: 24px;
+      height: 24px;
+      border-radius: 50%;
+      position: relative;
+      transform: translateZ(0);
+      margin-left: calc(50% + 12px);
+      border-left: 4px solid #ffffff;
+      animation: load8 1.1s infinite linear;
+      border-top: 4px solid rgba(255, 255, 255, 0.2);
+      border-right: 4px solid rgba(255, 255, 255, 0.2);
+      border-bottom: 4px solid rgba(255, 255, 255, 0.2);
+      &:after {
+         width: 24px;
+         height: 24px;
+         border-radius: 50%;
+      }
+      @-webkit-keyframes load8 {
+         0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+         }
+         100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+         }
+      }
+      @keyframes load8 {
+         0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+         }
+         100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+         }
+      }
+   `,
+}
