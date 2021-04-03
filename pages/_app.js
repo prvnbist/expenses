171cfd147b6461ca2f3254e9ba69cc333110b78c
Praveen Bisht <prvnbist@gmail.com>
2021-04-03 17:38:09 +0530

    feat: auth screen

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import {
    createHttpLink,
    ApolloProvider,
 } from '@apollo/client'
+import tw from 'twin.macro'
 import fetch from 'node-fetch'
 import { getMainDefinition } from '@apollo/client/utilities'
 import { WebSocketLink } from '@apollo/client/link/ws'
@@ -14,6 +15,7 @@ import { SubscriptionClient } from 'subscriptions-transport-ws'
 import '../styles/global.css'
 import { ConfigProvider } from '../context'
 import GlobalStyles from '../styles/global'
+import { Button } from '../components'
 
 const wssLink = process.browser
    ? new WebSocketLink(
@@ -63,14 +65,80 @@ const client = new ApolloClient({
 })
 
 const App = ({ Component, pageProps }) => {
+   const [authenticated, setAuthenticated] = React.useState(false)
+
+   React.useEffect(() => {
+      const secret = localStorage.getItem('secret')
+      if (secret && secret === process.env.HASURA_KEY) {
+         setAuthenticated(true)
+      }
+   }, [])
    return (
       <ApolloProvider client={client}>
          <GlobalStyles />
          <ConfigProvider>
-            <Component {...pageProps} />
+            {authenticated ? (
+               <Component {...pageProps} />
+            ) : (
+               <Login setAuthenticated={setAuthenticated} />
+            )}
          </ConfigProvider>
       </ApolloProvider>
    )
 }
 
 export default App
+
+const Login = ({ setAuthenticated }) => {
+   const [key, setKey] = React.useState('')
+   const [saveKey, setSaveKey] = React.useState(false)
+   const [error, setError] = React.useState('')
+
+   const onSubmit = () => {
+      if (!process.browser) return
+
+      if (process.env.HASURA_KEY === key) {
+         saveKey && localStorage.setItem('secret', key)
+         setAuthenticated(true)
+      } else {
+         setError('Incorrect key provided!')
+      }
+   }
+   return (
+      <div tw="flex items-center justify-center h-screen">
+         <section tw="bg-gray-900 py-3 px-4 rounded">
+            <fieldset tw="flex flex-col space-y-1 mt-2 flex-1">
+               <label
+                  htmlFor="key"
+                  tw="text-sm text-gray-500 uppercase font-medium tracking-wider"
+               >
+                  Key
+               </label>
+               <input
+                  value={key}
+                  type="password"
+                  placeholder="Enter the key"
+                  tw="bg-gray-700 h-10 rounded px-2"
+                  onChange={e => setKey(e.target.value)}
+               />
+            </fieldset>
+            <fieldset tw="my-3">
+               <input
+                  type="checkbox"
+                  id="remember"
+                  name="remember"
+                  value={saveKey}
+                  onChange={() => setSaveKey(!saveKey)}
+               />
+               <label htmlFor="remember" tw="ml-2 text-gray-500 ">
+                  Remember in this browser
+               </label>
+            </fieldset>
+            <Button.Text tw="w-full" onClick={onSubmit}>
+               Submit
+            </Button.Text>
+            <span tw="block pt-3 text-red-500">{error}</span>
+         </section>
+      </div>
+   )
+}
