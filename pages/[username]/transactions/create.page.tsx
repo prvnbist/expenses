5afe8666e148f37f96c7c6cb72b194577f1182e7
Praveen Bisht <prvnbist@gmail.com>
2022-01-15 13:21:48 +0530

    feat: transaction create/edit states

--- pages/[username]/transactions/create.page.tsx
+++ pages/[username]/transactions/create.page.tsx
@@ -9,6 +9,7 @@ import { useUser } from '../../../lib/user'
 import Layout from '../../../sections/layout'
 import QUERIES from '../../../graphql/queries'
 import { MUTATIONS } from '../../../graphql/mutations'
+import { Loader } from '../../../components'
 
 type Inputs = {
    title: string
@@ -45,6 +46,9 @@ const CreateTransaction = () => {
    const { user } = useUser()
    const router = useRouter()
    const FORM_TYPE = router.query.id ? 'EDIT' : 'CREATE'
+   const [status, setStatus] = React.useState(
+      FORM_TYPE === 'EDIT' ? 'LOADING' : 'SUCCESS'
+   )
    const [type, setType] = React.useState<'expense' | 'income'>('expense')
    const [selectedCategory, setSelectedCategory] =
       React.useState<ISelectedCategoryState | null>(null)
@@ -94,7 +98,9 @@ const CreateTransaction = () => {
          if (transaction.category_id) {
             setSelectedCategory({ value: transaction.category_id, label: '' })
          }
+         setStatus('SUCCESS')
       },
+      onError: () => setStatus('ERROR'),
    })
 
    React.useEffect(() => {
@@ -165,121 +171,135 @@ const CreateTransaction = () => {
       <Layout>
          <header tw="px-4 pt-4">
             <h1 tw="font-heading text-3xl font-medium text-gray-400">
-               Create Transaction
+               {FORM_TYPE === 'CREATE' ? 'Create' : 'Edit'} Transaction
             </h1>
          </header>
-         <form
-            onSubmit={handleSubmit(onSubmit)}
-            tw="w-full max-w-[380px] mt-4 px-4 space-y-3"
-         >
-            <fieldset>
-               <Styles.Label htmlFor="title">Title</Styles.Label>
-               <Styles.Text
-                  {...register('title', {
-                     required: true,
-                     minLength: 2,
-                     maxLength: 60,
-                  })}
-                  id="title"
-                  name="title"
-                  placeholder="Enter the title"
-               />
-               {errors.title?.type === 'required' && (
-                  <Styles.Error>Please fill the title</Styles.Error>
+         {status === 'LOADING' ? (
+            <Loader />
+         ) : (
+            <>
+               {status === 'ERROR' ? (
+                  <p tw="text-gray-400">
+                     Something went wrong, please try again.
+                  </p>
+               ) : (
+                  <form
+                     onSubmit={handleSubmit(onSubmit)}
+                     tw="w-full max-w-[380px] mt-4 px-4 space-y-3"
+                  >
+                     <fieldset>
+                        <Styles.Label htmlFor="title">Title</Styles.Label>
+                        <Styles.Text
+                           {...register('title', {
+                              required: true,
+                              minLength: 2,
+                              maxLength: 60,
+                           })}
+                           id="title"
+                           name="title"
+                           placeholder="Enter the title"
+                        />
+                        {errors.title?.type === 'required' && (
+                           <Styles.Error>Please fill the title</Styles.Error>
+                        )}
+                        {errors.title?.type === 'minLength' && (
+                           <Styles.Error>Title is too short</Styles.Error>
+                        )}
+                        {errors.title?.type === 'maxLength' && (
+                           <Styles.Error>Title is too long</Styles.Error>
+                        )}
+                     </fieldset>
+                     <fieldset>
+                        <Styles.Label htmlFor="amount">
+                           Amount{' '}
+                           <span tw="text-[12px] float-right normal-case">
+                              (Upto two decimals)
+                           </span>
+                        </Styles.Label>
+                        <Styles.Text
+                           {...register('amount', {
+                              required: true,
+                              pattern: /^[\d]{1,3}(\.[\d]{1,2})?$/,
+                           })}
+                           id="amount"
+                           name="amount"
+                           placeholder="Enter the amount"
+                        />
+                        {errors.amount?.type === 'required' && (
+                           <Styles.Error>Please fill the amount</Styles.Error>
+                        )}
+                        {errors.amount?.type === 'pattern' && (
+                           <Styles.Error>
+                              Please enter numbers only
+                           </Styles.Error>
+                        )}
+                     </fieldset>
+                     <fieldset>
+                        <Styles.Label htmlFor="date">Date</Styles.Label>
+                        <Styles.Text
+                           {...register('date', {
+                              required: true,
+                           })}
+                           id="date"
+                           name="date"
+                           type="date"
+                           placeholder="Enter the date"
+                        />
+                        {errors.date?.type === 'required' && (
+                           <Styles.Error>Please fill the date</Styles.Error>
+                        )}
+                     </fieldset>
+                     <div tw="flex items-center bg-dark-300 p-1 rounded-lg">
+                        <Styles.GroupButton
+                           is_selected={type === 'expense'}
+                           onClick={(e: React.FormEvent<HTMLInputElement>) => {
+                              e.preventDefault()
+                              setType('expense')
+                           }}
+                        >
+                           Expense
+                        </Styles.GroupButton>
+                        <Styles.GroupButton
+                           is_selected={type === 'income'}
+                           onClick={(e: React.FormEvent<HTMLInputElement>) => {
+                              e.preventDefault()
+                              setType('income')
+                           }}
+                        >
+                           Income
+                        </Styles.GroupButton>
+                     </div>
+                     <Select
+                        isClearable
+                        isSearchable
+                        name="category"
+                        isLoading={loading}
+                        classNamePrefix="select"
+                        value={selectedCategory}
+                        onChange={(option: any) => setSelectedCategory(option)}
+                        options={categories.map((category: ICategory) => ({
+                           label: category.title,
+                           options: category.sub_categories.map(
+                              (sub_category: ISubCategory) => ({
+                                 value: sub_category.id,
+                                 label: sub_category.title,
+                              })
+                           ),
+                        }))}
+                     />
+                     <button
+                        type="submit"
+                        disabled={creating_transaction || updating_transaction}
+                        tw="border border-dark-200 h-10 px-3 text-white hover:bg-dark-300 disabled:(cursor-not-allowed opacity-50 hover:bg-transparent)"
+                     >
+                        {creating_transaction || updating_transaction
+                           ? 'Saving...'
+                           : 'Save'}
+                     </button>
+                  </form>
                )}
-               {errors.title?.type === 'minLength' && (
-                  <Styles.Error>Title is too short</Styles.Error>
-               )}
-               {errors.title?.type === 'maxLength' && (
-                  <Styles.Error>Title is too long</Styles.Error>
-               )}
-            </fieldset>
-            <fieldset>
-               <Styles.Label htmlFor="amount">
-                  Amount{' '}
-                  <span tw="text-[12px] float-right normal-case">
-                     (Upto two decimals)
-                  </span>
-               </Styles.Label>
-               <Styles.Text
-                  {...register('amount', {
-                     required: true,
-                     pattern: /^[\d]{1,3}(\.[\d]{1,2})?$/,
-                  })}
-                  id="amount"
-                  name="amount"
-                  placeholder="Enter the amount"
-               />
-               {errors.amount?.type === 'required' && (
-                  <Styles.Error>Please fill the amount</Styles.Error>
-               )}
-               {errors.amount?.type === 'pattern' && (
-                  <Styles.Error>Please enter numbers only</Styles.Error>
-               )}
-            </fieldset>
-            <fieldset>
-               <Styles.Label htmlFor="date">Date</Styles.Label>
-               <Styles.Text
-                  {...register('date', {
-                     required: true,
-                  })}
-                  id="date"
-                  name="date"
-                  type="date"
-                  placeholder="Enter the date"
-               />
-               {errors.date?.type === 'required' && (
-                  <Styles.Error>Please fill the date</Styles.Error>
-               )}
-            </fieldset>
-            <div tw="flex items-center bg-dark-300 p-1 rounded-lg">
-               <Styles.GroupButton
-                  is_selected={type === 'expense'}
-                  onClick={(e: React.FormEvent<HTMLInputElement>) => {
-                     e.preventDefault()
-                     setType('expense')
-                  }}
-               >
-                  Expense
-               </Styles.GroupButton>
-               <Styles.GroupButton
-                  is_selected={type === 'income'}
-                  onClick={(e: React.FormEvent<HTMLInputElement>) => {
-                     e.preventDefault()
-                     setType('income')
-                  }}
-               >
-                  Income
-               </Styles.GroupButton>
-            </div>
-            <Select
-               isClearable
-               isSearchable
-               name="category"
-               isLoading={loading}
-               classNamePrefix="select"
-               value={selectedCategory}
-               onChange={(option: any) => setSelectedCategory(option)}
-               options={categories.map((category: ICategory) => ({
-                  label: category.title,
-                  options: category.sub_categories.map(
-                     (sub_category: ISubCategory) => ({
-                        value: sub_category.id,
-                        label: sub_category.title,
-                     })
-                  ),
-               }))}
-            />
-            <button
-               type="submit"
-               disabled={creating_transaction || updating_transaction}
-               tw="border border-dark-200 h-10 px-3 text-white hover:bg-dark-300 disabled:(cursor-not-allowed opacity-50 hover:bg-transparent)"
-            >
-               {creating_transaction || updating_transaction
-                  ? 'Saving...'
-                  : 'Save'}
-            </button>
-         </form>
+            </>
+         )}
       </Layout>
    )
 }
