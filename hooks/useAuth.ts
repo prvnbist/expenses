e04949f56aa5bde48c4b26926c66cc38e4ce13fb
Praveen Bisht <prvnbist@gmail.com>
2022-01-16 14:26:47 +0530

    fix: remove comment

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -9,16 +9,7 @@ function fetcher(route: string) {
 }
 
 export default function useAuth() {
-   // const { data: user, error } = useSWR('/api/user', fetcher)
-   let user: IUser = {
-      issuer: 'did:ethr:0xe81197FB3CfCDdb3b65259Ba10843632D70d52F1',
-      publicAddress: '0xe81197FB3CfCDdb3b65259Ba10843632D70d52F1',
-      email: 'prvnbist@gmail.com',
-      oauthProvider: null,
-      phoneNumber: null,
-      id: 'da1db57f-29fb-44ea-a857-7ab98c4077c5',
-      username: 'prvnbist',
-   }
+   const { data: user, error } = useSWR('/api/user', fetcher)
    const loading = user === undefined
 
    return {
