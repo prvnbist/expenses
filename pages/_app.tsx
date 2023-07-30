cd170fb3539465eb3e1bd06157b292b117da31ac
Praveen Bisht <prvnbist@gmail.com>
2023-07-30 12:23:26 +0530

    feat: mantine setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { MantineProvider } from '@mantine/core'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '@/styles/globals.css'
+
+export default function App(props: AppProps) {
+   const { Component, pageProps } = props
+
+   return (
+      <>
+         <Head>
+            <title>Expenses</title>
+            <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+         </Head>
+
+         <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={{
+               /** Put your mantine theme override here */
+               colorScheme: 'dark',
+            }}
+         >
+            <Component {...pageProps} />
+         </MantineProvider>
+      </>
+   )
 }
